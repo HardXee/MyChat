2ea692fa57ede14aa2f1df781c49083902ca595b
HardXee <adityathakur2425@gmail.com>
2026-06-01 00:55:33 +0530

    fixed chatsection

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import { joinRoom } from "../../socket/chatFunctions";
 
-import ChatSection from "../../components/chatSection/chatSection";
+import ChatSection from "../../components/chatSection/chatSection.jsx";
 import axios from "axios";
 
 function Chat() {
